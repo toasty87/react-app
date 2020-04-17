ff4ca3ebaaeef77513afc2998601c4bf5830298b
Justin Packer <justin.packer@iptelx.net>
2020-04-17 12:27:27 -0400

    fix: add more core messages

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -3714,13 +3714,24 @@ const github = __webpack_require__(469);
 
 try {
 //  throw( new Error("some error message"));
+
+  core.debug('Debug message');
+  core.warning('Warning message');
+  core.error('Error message');
+
   const name = core.getInput('who-to-greet');
+  core.setSecret(name); //look like *** in logs
   console.log(`Hello ${name}`);
 
   const time = new Date();
   core.setOutput('time', time.toTimeString());
 
+  core.startGroup('Logging github object');
+
   console.log(JSON.stringify(github, null, '\t'));
+  core.endGroup();
+
+  core.exportVariable('Hello', 'hello');
 } catch(error) {
   core.setFailed(error.message)
 }
--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -3,13 +3,24 @@ const github = require('@actions/github');
 
 try {
 //  throw( new Error("some error message"));
+
+  core.debug('Debug message');
+  core.warning('Warning message');
+  core.error('Error message');
+
   const name = core.getInput('who-to-greet');
+  core.setSecret(name); //look like *** in logs
   console.log(`Hello ${name}`);
 
   const time = new Date();
   core.setOutput('time', time.toTimeString());
 
+  core.startGroup('Logging github object');
+
   console.log(JSON.stringify(github, null, '\t'));
+  core.endGroup();
+
+  core.exportVariable('Hello', 'hello');
 } catch(error) {
   core.setFailed(error.message)
 }
